93f1391c61bc4cccdc7c3b2408ebab693686ff0d
Nathan Shy <nathan.shy@via-appia.ph>
2024-03-19 19:19:17 +0800

    first commit

---
+++ intro/basic.dart
@@ -0,0 +1,5 @@
+void main() {
+  var firstName = "Nathan";
+  var lastName = "Shy";
+  print("Full Name is: $firstName $lastName");
+}
---
+++ intro/variables.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = "Nathan";
+  String address = "Philippines";
+  num age = 20;
+  num height = 5.9;
+  bool isMarried = false;
+
+  print("name is $name");
+  print("Address is $address");
+  print("Age is $age");
+  print("Height is $height");
+  print("Marriage status is $isMarried");
+}
