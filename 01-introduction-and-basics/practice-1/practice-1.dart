6bde5628e61e26d3ef9647359fa4c108bccc5060
Nathan Shy <nathan.shy@via-appia.ph>
2024-03-20 18:55:32 +0800

    finished practice-1 (rev)

--- 01-introduction-and-basics/practice-1/practice-1.dart
+++ 01-introduction-and-basics/practice-1/practice-1.dart
@@ -65,7 +65,6 @@ void main() {
   ///Suppose, your distance to office from home is 25 km and you travel 40 km per hour. Write a program to calculate time taken to reach office in minutes. Formula= (distance) / (speed)
   int distance = 25;
   int speed = 40;
-
   print("Time taken to reach office: ${(distance/speed)*60} minutes");
   
 }
