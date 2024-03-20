ed719a193ff321acdcbdcd41135f7b8fde680fbf
Nathan Shy <Nathan Shy>
2024-03-20 11:57:55 +0800

    updated practice-1

---
+++ 01-introduction-and-basics/practice-1/practice-1.dart
@@ -0,0 +1,41 @@
+void main() {
+  /// Write a program to print your name in Dart.
+  print("Nathan Shy");
+
+  /// Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.
+  print('Hello I am "John Doe"');
+  print('''Hello I'm "John Doe"''');
+
+  ///Declare constant type of int set value 7.
+  const int value = 7;
+
+  ///Write a program in Dart that finds simple interest. Formula= (p * t * r) / 100
+  double principal = 1000.00;
+  int time = 1; // in years
+  double rate = 5; //5% interest
+  double simpleInterest = (principal*time*rate)/ 100;
+
+  print("Simple interest is: $simpleInterest");
+
+  ///Write a program to print full name of a from first name and last name using user input.
+
+
+  ///Write a program to find quotient and remainder of two integers.
+
+
+  ///Write a program to swap two numbers.
+  
+
+  ///Write a program in Dart to remove all whitespaces from String.
+  
+
+  ///Write a Dart program to convert String to int.
+  
+
+  ///Suppose, you often go to restaurant with friends and you have to split amount of bill. Write a program to calculate split amount of bill. Formula= (total bill amount) / number of people
+  
+
+  ///Suppose, your distance to office from home is 25 km and you travel 40 km per hour. Write a program to calculate time taken to reach office in minutes. Formula= (distance) / (speed)
+  
+  
+}
