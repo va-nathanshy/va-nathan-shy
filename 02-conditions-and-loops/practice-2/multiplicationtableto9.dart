447c2bb04a20b33e17c6c86d4837045f72a400fe
Nathan Shy <Nathan Shy>
2024-03-25 14:28:36 +0800

    mostly done, need to add exception handling for better design

--- 02-conditions-and-loops/practice-2/multiplicationtableto9.dart
+++ 02-conditions-and-loops/practice-2/multiplicationtableto9.dart
@@ -1,4 +1,20 @@
+import "dart:io";
+
 void main() {
 /// Write a dart program to generate multiplication tables of 1-9.
+while(true){
+stdout.write("How many rows to print? ");
+int rows = int.parse(stdin.readLineSync()!);
+
+  for(int i = 1; i <= 9; i++){
+    print("MULTIPLICATION TABLE FOR ${i}");
+    for(int j = 1; j <= rows; j++){
+      print("${j} * ${i} = ${j*i}");
+    }
+    print("\n");
+  }
+}
+
+
 
 }
