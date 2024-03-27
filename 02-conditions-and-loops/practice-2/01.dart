4cf99ce900a9957eddcef397163d40d9116a776e
Nathan Shy <Nathan Shy>
2024-03-27 16:02:33 +0800

    updated practice-2

--- 02-conditions-and-loops/practice-2/01.dart
+++ 02-conditions-and-loops/practice-2/01.dart
@@ -1,5 +1,10 @@
 void main() {
 /// Write a dart program to check if the number is odd or even.
+  int num = 13;
 
-
+  if(num % 2 == 0) {
+    print("Number is even");
+  } else {
+    print("Number is odd");
+  }
 }
