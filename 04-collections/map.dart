9ab97ca44a6697183bdc212dfd8cf277a63f7ef6
Nathan Shy <nathan.shy@via-appia.ph>
2024-03-27 09:29:01 +0800

    updated 04

---
+++ 04-collections/map.dart
@@ -0,0 +1,23 @@
+void main(){
+  // Map<String, String> userData = {
+  //   "Name": "Nathan",
+  //   "Address" : "Quezon City",
+  //   "Gender" : "M"
+  // };
+
+  //print(userData);
+
+  // Access value from key
+  // variableName[key]
+  print(userData["Name"]);
+
+  // Map Properties
+  // keys	To get all keys.
+  // values	To get all values.
+  // isEmpty	Return true or false.
+  // isNotEmpty	Return true or false.
+  // length	It returns the length of the Map.
+
+
+
+}
