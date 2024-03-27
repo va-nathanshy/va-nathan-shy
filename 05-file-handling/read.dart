dbc067f4309b5bc97cff855b5a8b14ee53f66cf8
Nathan Shy <nathan.shy@via-appia.ph>
2024-03-27 19:33:57 +0800

    updated 05

---
+++ 05-file-handling/read.dart
@@ -0,0 +1,35 @@
+import "dart:io";
+
+void main() {
+  // Read file
+  // File file = File('text.txt');
+  // String contents = file.readAsStringSync();
+  // print(contents);
+
+  // Get File information
+  // File file = File('text.txt');
+  // print("File Path: ${file.path}");
+  // print("File Absolute Path: ${file.absolute.path}");
+  // print("File Size: ${file.lengthSync()} bytes");
+  // print("Last Modified: ${file.lastModifiedSync()}");
+
+  // Read CSV File
+  // File file = File('text.csv');
+
+  // String contents = file.readAsStringSync();
+
+  // List<String> lines = contents.split("\n");
+
+  // print("---------------");
+  // for(var line in lines) {
+  //   print(line);
+  // }
+
+  // Read Only Part of File
+  // File file = File('text.txt');
+  // String contents = file.readAsStringSync().substring(0,10);
+  // print(contents);
+
+  // Read file from specific directory
+  // File file = File(Directory)
+}
