2b32c256b4ec8e8fcf9182e8351ea49a4d597458
Nathan Shy <nathan.shy@via-appia.ph>
2024-04-02 11:24:25 +0800

    updated 06

--- 06-oop/04-abstraction/21-generic-main.dart
+++ 06-oop/04-abstraction/21-generic-main.dart
@@ -1,1 +1,84 @@
+import '21-generic.dart';
+
 /// Without Using Generics
+// void main() {
+//   // Create an object of IntData class
+//   IntData intData = IntData(10);
+//   DoubleData doubleData = DoubleData(10.5);
+
+//   // Print the data
+//   print("IntData: ${intData.data}");
+//   print("DoubleData: ${doubleData.data}");
+// }
+
+
+/// Using Generics
+// void main() {
+//   // create an object of type int and double
+//   Data<int> intData = Data<int>(10);
+//   Data<double> doubleData = Data<double>(10.5);
+
+//   // print the data
+//   print("IntData: ${intData.data}");
+//   print("DoubleData: ${doubleData.data}");
+// }
+
+
+/// Dart Map Class
+// void main() {
+//   final info ={
+//     "name": "John",
+//     "age": 20,
+//     "height": 5.5
+//   };
+
+//   print(info);
+// }
+
+/// Generics Methods
+// void main() {
+//   // Call the generic method
+//   print("Int: ${genericMethod<int>(10)}");
+//   print("Double: ${genericMethod<double>(10.5)}");
+//   print("String: ${genericMethod<String>("Hello")}");
+// }
+
+
+/// Generic Method with Multiple Parameters
+// void main() {
+// print(genericMethod<int, String>(10, "Hello"));
+// print(genericMethod<String, int>("Hello", 10));
+// }
+
+
+/// Generic Class with Restriction
+// void main() {
+//   // Create and object of type int and double
+//   Data<int> intData = Data<int>(10);
+//   Data<double> doubleData = Data<double>(10.5);
+//   // Data<String> stringData = Data<String>("Hello"); // Will not work
+
+//   // print the data
+//   print("IntData: ${intData.data}");
+//   print("DoubleData: ${doubleData.data}");
+// }
+
+
+/// Generic Method with Restriction
+// call the generic method
+// void main() {
+//   print("Average of int: ${getAverage<int>(10,20)}");
+//   print("Average of double: ${getAverage<double>(10.5,20.5)}");
+// }
+
+
+/// Generic class
+void main() {
+  var circle = Circle(10);
+  var rectangle = Rectangle(10,20);
+
+  // create a list of shape objects
+  var region = Region(shapes: [circle, rectangle]);
+
+  print("Total Area of Region: ${region.totalArea}");
+}
--- 06-oop/04-abstraction/21-generic.dart
+++ 06-oop/04-abstraction/21-generic.dart
@@ -1,4 +1,100 @@
 /// Without Using Generics
-void main() {
+// Creating a class for int
+// class IntData {
+//   int data;
+//   IntData(this.data);
+// }
+
+// // Creating a class for double
+// class DoubleData {
+//   double data;
+//   DoubleData(this.data);
+// }
+
+
+/// Using Generics
+// class Data<T> {
+//   T data;
+//   Data(this.data);
+// }
+
+
+/// Dart Map Class
+// abstract class Map<K, V> {
+//   // code
+//   external factory Map();
+// }
+
+
+/// Generics Methods
+// T genericMethod<T> (T value) {
+//   return value;
+// }
+
+
+/// Generic Method with Multiple Parameters
+// T genericMethod<T, U>(T value1, U value2) {
+//   return value1;
+// }
+
+
+/// Generic Class with Restriction
+// Define generic class with bounded type
+// class Data<T extends num> {
+//   T data;
+//   Data(this.data);
+// }
+
+
+/// Generic Method with Restriction
+// Define generic method
+// double getAverage<T extends num>(T value1, T value2) {
+//   return (value1 + value2) / 2;
+// }
+
+
+/// Generic class
+abstract class Shape {
+  double get area;
+}
+
+class Circle implements Shape {
+  // Field Radius
+  final double radius;
   
+  // Constructor
+  Circle(this.radius);
+
+  // implementation of area method
+  @override
+  double get area => 3.14 * radius * radius;
+}
+
+class Rectangle implements Shape {
+  final double width;
+  final double height;
+
+  // Constructor
+  Rectangle(this.width, this.height);
+
+  // Implementation of area method
+  @override
+  double get area => width * height;
+}
+
+// Generic class Region
+class Region<T extends Shape> {
+  List<T> shapes;
+
+  // constructor
+  Region({required this.shapes});
+
+  // method to get totalArea
+  double get totalArea {
+    double total = 0;
+    shapes.forEach((shape) {
+      total += shape.area;
+    });
+    return total;
+  }
 }
